d64fb5a6f8d97e2370f132912a7cd6ec7225fd78
daniel llach <danielllach@iMac-de-daniel.local>
2015-01-07 10:21:59 -0300

    hola mundo node con server

--- mis_ejercicios/hello_world/app.js
+++ mis_ejercicios/hello_world/app.js
@@ -1,1 +1,10 @@
-console.log('Hola, mundo!');
+var http = require('htpp');
+
+var server = http.createServer(function (request, response){
+	response.writeHead(200, {'content-Type': 'text/plain'});
+	response.end('Hola, mundo'\n);
+});
+
+server.listen(8000);
+
+console.log('Servidor corriendo en http://localhost:8000');

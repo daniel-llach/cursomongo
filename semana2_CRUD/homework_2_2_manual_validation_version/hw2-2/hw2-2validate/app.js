3373060b858a5a3d06e8f1f27688e4d43357bdec
daniel llach <danielllach@iMac-de-daniel.local>
2015-01-21 10:34:54 -0300

    [FIX] app.js homework2_2

--- semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
+++ semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
@@ -4,18 +4,20 @@ var currentState;
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     if(err) throw err;
 
-    var data = db.collection('data');
+    console.log('conectado');
 
-    var options = { 'sort' : [['State', 1], ['Temperature', -1]] };
-    var cursor = data.find({}, {}, options);
+    // var data = db.collection('data');
 
-    cursor.each(function(err, doc) {
-        if(err) throw err;
-        if(doc == null) {
-            return db.close();
-        }else if(currentState == null || currentState != doc.State){
-            db.collection('data').update({"_id": doc._id}, {"$set":{"month_high":true}});
-            currentState = doc.State;
-        }
-    });
+    // var options = { 'sort' : [['State', 1], ['Temperature', -1]] };
+    // var cursor = data.find({}, {}, options);
+
+    // cursor.each(function(err, doc) {
+    //     if(err) throw err;
+    //     if(doc == null) {
+    //         return db.close();
+    //     }else if(currentState == null || currentState != doc.State){
+    //         db.collection('data').update({"_id": doc._id}, {"$set":{"month_high":true}});
+    //         currentState = doc.State;
+    //     }
+    // });
 });

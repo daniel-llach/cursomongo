47fe664bbbc1b3b52b79f627b2cb59aa3c490aa3
daniel llach <danielllach@iMac-de-daniel.local>
2015-01-21 09:44:51 -0300

    [FIX] app.js homew2_2

--- semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
+++ semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
@@ -3,10 +3,10 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     if(err) throw err;
 
-    var grades = db.collection('grades');
+    var data = db.collection('data');
 
     var options = { 'sort' : [['State', 1], ['Temperature', -1]] };
-    var cursor = grades.find({}, {}, options);
+    var cursor = data.find({}, {}, options);
 
     cursor.each(function(err, doc) {
         if(err) throw err;

3073cd384a9740f2bb18a03e954683a258f39e77
daniel llach <danielllach@iMac-de-daniel.local>
2015-01-21 10:39:47 -0300

    [FIX] app.js homework2_2

--- semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
+++ semana2_CRUD/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
@@ -11,14 +11,15 @@ MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     var options = { 'sort' : [['State', 1], ['Temperature', -1]] };
     var cursor = data.find({}, {}, options);
 
-    console.log(cursor);
-    // cursor.each(function(err, doc) {
-    //     if(err) throw err;
-    //     if(doc == null) {
-    //         return db.close();
-    //     }else if(currentState == null || currentState != doc.State){
-    //         db.collection('data').update({"_id": doc._id}, {"$set":{"month_high":true}});
-    //         currentState = doc.State;
-    //     }
-    // });
+    cursor.each(function(err, doc) {
+
+        console.log(err);
+        // if(err) throw err;
+        // if(doc == null) {
+        //     return db.close();
+        // }else if(currentState == null || currentState != doc.State){
+        //     db.collection('data').update({"_id": doc._id}, {"$set":{"month_high":true}});
+        //     currentState = doc.State;
+        // }
+    });
 });
